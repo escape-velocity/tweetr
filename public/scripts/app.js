a33dd79b8f64ef14c376156a5a96c3584e2ffb4b
Bill <networker747@gmail.com>
2017-05-19 20:03:33 +0000

    changes to add toggle and hide functionality

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,59 +1,6 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
-// console.log('im working')
+
 // Document ready
 $(function() {
-  // var data = [
-  //   {
-  //     "user": {
-  //       "name": "Newton",
-  //       "avatars": {
-  //         "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-  //         "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-  //         "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-  //       },
-  //       "handle": "@SirIsaac"
-  //     },
-  //     "content": {
-  //       "text": "If I have seen further it is by standing on the shoulders of giants"
-  //     },
-  //     "created_at": 1461116232227
-  //   },
-  //   {
-  //     "user": {
-  //       "name": "Descartes",
-  //       "avatars": {
-  //         "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
-  //         "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
-  //         "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
-  //       },
-  //       "handle": "@rd" },
-  //     "content": {
-  //       "text": "Je pense , donc je suis"
-  //     },
-  //     "created_at": 1461113959088
-  //   },
-  //   {
-  //     "user": {
-  //       "name": "Johann von Goethe",
-  //       "avatars": {
-  //         "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
-  //         "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
-  //         "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
-  //       },
-  //       "handle": "@johann49"
-  //     },
-  //     "content": {
-  //       "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
-  //     },
-  //     "created_at": 1461113796368
-  //   }
-  // ];
-
-
   function createTweetElement(tweet) {
     const { name, handle } = tweet.user;
     const html = `
@@ -78,7 +25,7 @@ $(function() {
   }
 
 
-  // loops through tweets
+  // This loops through
   function renderTweets(tweets) {
     var $tweetContainer = $('#allTweets');
     $tweetContainer.empty();
@@ -88,21 +35,11 @@ $(function() {
     // console.log('tweets');
 
 
-    // calls createTweetElement for each tweet
+    // This one calls createTweetElement for each tweet
     // takes return value and appends it to the tweets container
   }
 
- function createNewTweet(data) {
-
-    $.ajax({
-      url: '/tweets',
-      type: 'POST',
-      data: data,
-      success: loadTweets
-    });
-  }
-
-function loadTweets() {
+  function loadTweets() {
     $.ajax({
       url: '/tweets',
       type: 'GET',
@@ -114,7 +51,15 @@ function loadTweets() {
   }
   loadTweets();
 
+  function createNewTweet(data) {
 
+    $.ajax({
+      url: '/tweets',
+      type: 'POST',
+      data: data,
+      success: loadTweets
+    });
+  }
 
   $('form').submit(function(evt) {
     evt.preventDefault();
@@ -130,19 +75,21 @@ function loadTweets() {
     }
   });
 
-  renderTweets(data);
 
-    $(".new-tweet").hide();
+
+
 
     $("button").click(function() {
       $(".new-tweet").slideToggle();
       $('textarea').focus();
       $("body").scrollTop(0);
-  });
+    });
+
+   $(".new-tweet").hide();
 
   function escape(str) {
     var div = document.createElement('div');
     div.appendChild(document.createTextNode(str));
-  return div.innerHTML;
-}
+    return div.innerHTML;
+  }
 });
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,43 +1,26 @@
 
 
-// $("textarea").keyup(function(){
-//   $(".counters").text("Characters left: " + (140 - $(this).val().length));
-// });
-// console.log();
-
-
-
-// $( document ).ready(function() {
-//   $('textarea').on('input', function() {
-//     var max = 140;
-//     var length =  max-($(this).val().length);
-//     $(this).parent().find('.counter').text(length);
-//     if (length <= 0) {
-//       $(this).parent().find('.counter').addClass('redmax');
-//     } else {
-//       $(this).parent().find('.counter').removeClass('redmax');
-//     }
-//   });
-// });
-
 // $(document).ready(function(){
 //   $('textarea').keyup(function(){
 //     var inputLength = $(this).val().length;
 //     var remain = 140 - inputLength;
 //     $('.counter').html(remain);
-//     if(remain>= 0){
-//     $('.counter').css('color', 'black');
+//     if(remain >= 0){
+//       $('.counter').css('color','black');
 //     } else {
-//     $('.counter').css('color', 'red');
+//       $('.counter').css('color','red');
 //     }
 //   });
 // });
 
 
+
 $(function(){
   $('.container .new-tweet textarea').keyup(function(){
     const inputLength = $(this).val().length;
+    console.log("bill", inputLength)
     const $counter = $('.new-tweet .counter').text(140 - inputLength);
+    console.log($counter)
     if(inputLength <= 140) {
       $counter.css('color', 'black');
     } else {
